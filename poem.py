e56f971b78c4b54f411d09778f6f641f79975af0
sandycamilo <scam499@lasierra.edu>
2019-11-04 15:45:30 -0800

    rearrange function

--- poem.py
+++ poem.py
@@ -32,25 +32,20 @@ def lines_printed_random(string_list):
     Randomly selects lines from a list of strings and prints them out in random order
     """
 
-    for string in string_list:
-       random_index = random.randint(0,len(string_list)-1) 
-       print(string_list[random_index])
-       
+    for _ in string_list:
+        print(random.choice(string_list))
 
+    #    random_index = random.randint(0,len(string_list)-1) 
+    #    print(string_list[random_index])
+
+ 
 def list_rearrange(string_list):
     """
-    Prints each line backwards
+    Prints each line in reverse
     """
-    string_list= """So the sun is shining blindingly but I can sort of see
-    It’s like looking at Mandela’s moral beauty
-    The dying leaves are sizzling on the trees
-    In a shirtsleeves summer breeze
-    But daylight saving is over
-    And gaveling the courtroom to order with a four-leaf clover
-    Is over. And it’s altogether November.
-    And the Pellegrino bubbles rise to the surface and dismember""" [::-1]
 
-    print(string_list)
+    for line in string_list:
+        print(line[::-1])
 
 
 lines_printed_backwards(string_list)
